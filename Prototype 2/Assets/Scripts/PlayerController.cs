382e6f49c8167fcda14a23d44c9101fff541fde9
Manasseh <manassehsarfo@yahoo.co.uk>
2022-08-22 14:37:44 +0100

    Prototype 2

---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    //Boundary variable
+    public float rightBoundary = 15;
+    public float leftBoundary = -15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < leftBoundary)
+        {
+            transform.position = new Vector3(leftBoundary, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > rightBoundary)
+        {
+            transform.position = new Vector3(rightBoundary, transform.position.y, transform.position.z);
+        }
+        //Get the user input
+        horizontalInput = Input.GetAxis("Horizontal");
+        //Move the player
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+    }
+}
